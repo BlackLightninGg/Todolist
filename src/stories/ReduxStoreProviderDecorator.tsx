cde59b4c2774869221455b2d63ca844777a2d4ae
Aleksey <easy.mail.to.copy@gmail.com>
2023-06-30 21:08:02 +0300

    Installed axios lib, created and learned get and post requests

--- src/stories/ReduxStoreProviderDecorator.tsx
+++
@@ -1,6 +0,0 @@
-import {Provider} from "react-redux";
-import {store} from "../state/store";
-
-export const ReduxStoreProviderDecorator = (storyFn: () => React.ReactNode) => {
-    return <Provider store={store}>{storyFn()}</Provider>
-}
